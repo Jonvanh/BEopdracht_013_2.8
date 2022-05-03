a64a4fe2e2452b2190a754bbbfb191a5e888e545
Jonvanh <78123488+Jonvanh@users.noreply.github.com>
2022-05-03 20:17:29 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -24,8 +24,8 @@ print(greet("jon", "hello dude"))               # hello dude
 print(greet("Bob", "What's up, <name>!"))       # What's up, Bob!
 
 
-# Part 2: Force
 
+# Part 2: Force
 
 def force(mass, body='earth'):
     body_dictionary = {
@@ -47,20 +47,20 @@ def force(mass, body='earth'):
     elif body not in body_dictionary:
         return f"The gravity of {body} is not in our dictionary. The name of the body should be provided with correct spelling and without any capitals"
     else:
-        bodygravity = body_dictionary.get(body)
-        return mass * bodygravity
+        return round(mass * body_dictionary[body], 2)
 
-print(force(1.0))
+print(force(0.1))                   # 0.98
 print(force(1, "earth"))            # The mass should be provided as a float
 print(force(0.1, "Earth"))          # The gravity of {body} is not in our dictionary. The name of the body should be provided with correct spelling and without any capitals
-print(force(0.1, "earth"))          # 0.9800000000000001
+print(force(0.1, "earth"))          # 0.98
 print(force(37.6, "neptune"))       # 421.12
 print(force(3846.6, "mercury"))     # 14232.42
 
+
+
 # Part 3: Gravity
 
-
-def pull(m1, m2, d):
+def pull(m1:float, m2:float, d:float) -> float:
     Isfloat_m1 = isinstance(m1, float)
     Isfloat_m2 = isinstance(m2, float)
     Isfloat_d = isinstance(d, float)
@@ -73,17 +73,14 @@ def pull(m1, m2, d):
 
     d2 = d * d
     g = 6674 * (10**-11)
-    #print(g)
     res = m1 * m2
-    #print(m_com)
     res1 = res / d2
-    #print(res)
     res2 = g * res1
-    return res2
+    return round(res2, 10)
 
 
 print(pull(6, 22.4, 6.9))                                           # The first object mass should be provided as a float
 print(pull(6.1, 22, 6.9))                                           # The second object should be provided as a float
 print(pull(6.1, 22.4, 7))                                           # The distance in meters should be provided as a float
-print(pull(800.0, 1500.0, 3.0))                                     # 0,000009 
-print(pull(0.1, 5972000000000000000000000000.0, 6371000000.0))      # 0.98
+print(pull(800.0, 1500.0, 3.0))                                     # 0.0088986667
+print(pull(0.1, 5972000000000000000000000000.0, 6371000000.0))      # 0.9819532033
